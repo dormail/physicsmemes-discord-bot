17adbf6d6aa0a725ac47525557b1dff2e2be59a5
mmaile <matthias.maile@pm.me>
2021-12-19 16:31:17 +0100

    Quasi fertig /s

--- example_bot.py
+++ example_bot.py
@@ -3,4 +3,16 @@ from secrets import TOKEN
 
 client = discord.Client()
 
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('meme pls'):
+        await message.channel.send('Hier sollte ein Meme stehen.')
+
 client.run(TOKEN)
